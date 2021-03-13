fe6cb3d71535c79ebe6d30324189688840b2d100
Krish <toonart.ru@gmail.com>
2021-03-13 16:34:04 +0300

    JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,15 +9,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
-        userService.dropUsersTable();
         userService.createUsersTable();
+
         userService.saveUser("Андрей", "Петров", (byte)23);
         userService.saveUser("Мария", "Балашова", (byte)18);
         userService.saveUser("Михаил", "Топоров", (byte)41);
         userService.saveUser("Кирилл", "Парфенов", (byte)30);
+
         List<User> allUsers = userService.getAllUsers();
         for (User u : allUsers) {
             System.out.println(u);
         }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
